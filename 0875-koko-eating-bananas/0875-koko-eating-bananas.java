7d339fea217553dff689b768e839a7c583d9ad41
Seongik Kim <kpic1638@gmail.com>
2023-09-01 22:13:16 +0900

    Time: 30 ms (31.65%), Space: 44.8 MB (9.85%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+/*
+ * Binary search from 1 to m(max in piles) to find minimum workable speed
+ */
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int hourSpent = 0;
+
+            for (int pile : piles){
+                hourSpent += Math.ceil((double) pile / mid);
+            }
+
+            // workable. Lower speed.
+            if (hourSpent <= h){
+                right = mid;
+            } else{ // not workable. Higher speed.
+                left = mid+1;
+            }
+        }
+        return right; // or left or anything
+    }
+}
